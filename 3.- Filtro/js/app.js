1553cd9024cfbc1132d6cff4774dfb2b1ce94bf2
VictorManuel2 <victorortizalvarez2530@gmail.com>
2025-01-15 19:42:31 -0600

    Funcionamiento de los filtros - practica terminada

--- 3.- Filtro/js/app.js
+++ 3.- Filtro/js/app.js
@@ -26,33 +26,38 @@ marca.addEventListener('change', e => {
     filtrarProducto();
 });
 minimo.addEventListener('change', e => {
-    objectoBusqueda.minimo = e.target.value;
+    objectoBusqueda.minimo = parseInt(e.target.value);
 
+    filtrarProducto();
 });
 maximo.addEventListener('change', e => {
     objectoBusqueda.maximo = e.target.value;
 
+    filtrarProducto();
 });
 memoria.addEventListener('change', e => {
-    objectoBusqueda.memoria = e.target.value;
+    objectoBusqueda.memoria = parseInt(e.target.value);
 
+    filtrarProducto();
+    
 });
 condicion.addEventListener('change', e => {
     objectoBusqueda.condicion = e.target.value;
 
+    filtrarProducto();
 });
 puertos.addEventListener('change', e => {
-    objectoBusqueda.puertos = e.target.value;
+    objectoBusqueda.puertos = parseInt(e.target.value);
 
+    filtrarProducto();
 });
 pantalla.addEventListener('change', e => {
     objectoBusqueda.pantalla = e.target.value;
 
-    console.log(objectoBusqueda)
+    filtrarProducto();
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-    
     mostrarEquipos(productos);    
 });
 
@@ -60,9 +65,9 @@ document.addEventListener('DOMContentLoaded', () => {
 function mostrarEquipos(productos){
 
     limpiarHTML();
-
+    
     productos.forEach( producto => {
-        const {marca, precio, memoriaRam, condicion, puertos, tipoPantalla} = producto;
+        const {imagen,marca, precio, memoriaRam, condicion, puertos, tipoPantalla} = producto;
         
         const cards = document.createElement('DIV');
         cards.classList.add('card');
@@ -73,21 +78,27 @@ function mostrarEquipos(productos){
         const info = document.createElement('DIV');
 
         const infoParrafo = document.createElement('P');
-        infoParrafo.textContent = `
-            ${marca} pantalla ${tipoPantalla}, ${puertos} entrada(s) HDMI, ${memoriaRam}GB RAM (${condicion}) - Precio $${precio}
+        infoParrafo.innerHTML += `
+            <img src="${imagen}" class="produc-img"/>
+            <p><strong>${marca}</strong> pantalla ${tipoPantalla} Monitor</p>
+            <p>${memoriaRam}GB RAM</p>
+            <p>${puertos} Entradas HDMI</p>
+            <strong class="condicion">(${condicion})</strong>
+            <strong>$${precio}°°</strong>
         `;
 
         card.appendChild(info);
         info.appendChild(infoParrafo);
-
-        resultado.appendChild(card)
+        resultado.appendChild(card);
     });
 }
 function filtrarProducto(){
-    const resultado = productos.filter(filtrarMarca);
-
+    const resultado = productos.filter(filtrarMarca).filter(filtarMinimo).filter(filtrarMaximo).filter(filtrarRam).filter(filtrarCondicion).filter(filtrarPuertos).filter(filtrarTipoPantalla);
+    
     if(resultado.length){
         mostrarEquipos(resultado);
+    }else{
+        mostrarMensaje();
     }
 }
 
@@ -98,7 +109,60 @@ function filtrarMarca(producto){
     }
     return producto
 }
+function filtarMinimo(producto){
+    const {minimo} = objectoBusqueda;
+    if(minimo){
+        return producto.precio >= minimo;
+    }
+    return producto
+}
+function filtrarMaximo(producto){
+    const {maximo} = objectoBusqueda;
+    if(maximo){
+        return producto.precio <= maximo;
+    }
+    return producto
+}
+function filtrarRam(producto){
+    const {memoria} = objectoBusqueda;
+    if(memoria){
+        return producto.memoriaRam === memoria;
+    }
+    return producto
+}
+function filtrarCondicion(producto){
+    const {condicion} = objectoBusqueda;
+    if(condicion){
+        return producto.condicion === condicion;
+    }
+    return producto
+}
+function filtrarPuertos(producto){
+    const {puertos} = objectoBusqueda;
+    if(puertos){
+        return producto.puertos === puertos;
+    }
+    return producto
+}
+function filtrarTipoPantalla(producto){
+    const {pantalla} = objectoBusqueda;
+    if(pantalla){
+        return producto.tipoPantalla === pantalla;
+    }
 
+    return producto
+}
+
+function mostrarMensaje(){
+
+    limpiarHTML();
+
+    const alerta = document.createElement('P');
+    alerta.classList.add('alerta');
+    alerta.textContent = 'No hay resultados con los filtros seleccionados';
+
+    resultado.appendChild(alerta)
+}
 function limpiarHTML(){
     while(resultado.firstChild){
         resultado.removeChild(resultado.firstChild)
